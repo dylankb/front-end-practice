a51f858719f419a313eb7b6b43354c137a8ae8a6
dylankb <dylan.barnard@gmail.com>
2016-07-16 23:10:26 -0700

    JS and jquery practice

--- js/calculator/js/scripts.js
+++ js/calculator/js/scripts.js
@@ -14,7 +14,21 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter your weight in pounds:"));
-var number2 = parseInt(prompt("Enter your height in centimeters:"));
-var result = divide(number1, number2);
-alert(result);
+$(document).ready(function() {
+  $('form#calculator').submit(function(event) {
+    event.preventDefault()
+    var number1 = parseInt($('#number1').val());
+    var number2 = parseInt($('#number2').val());
+    var operation = $('#operation').val()
+
+    var result = add(number1, number2);
+    var message = operation.concat(" ", number1, " and ", number2, " equals ")
+    var output = message + result.toString();
+
+    $('.output').text(output)
+  });  
+});
+
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+// var operation = prompt("Enter an operation");
--- js/jquery/form-grab/js/scripts.js
+++ js/jquery/form-grab/js/scripts.js
@@ -4,10 +4,15 @@ $(document).ready(function() {
     var firstName = $("#first-name").val();
     var lastName = $("#last-name").val();
     var age = $("#age").val();
+    var beverage = parseInt($("#beverage").val());
+
 
     $(".first-name").text(firstName)
     $(".last-name").text(lastName);
     $(".age").text(age);
+    $(".beverage").text(beverage);
+
+    // alert(typeof beverage)
 
     $(".intro").show();
 
