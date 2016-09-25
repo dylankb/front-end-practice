fd49238b398541f92af733db2624e9fc138decc3
Dylan Barnard <dylan.barnard@gmail.com>
2016-09-24 17:32:41 -0700

    JS DOM and variable scopingpractice

--- js/ls/220/lesson_3/1_dom.js
+++ js/ls/220/lesson_3/1_dom.js
@@ -5,12 +5,28 @@ var url = "http://d186loudes4jlv.cloudfront.net/fe2/exercises_objects_and_dom/do
 document.getElementById('primary_heading').setAttribute('class', 'heading');
 
 // 3. Click function to toggle class
-document.getElementById('toggle').onclick = function() {
-  target = document.getElementById('notice');
-  if (target.className === "hidden") {
-    target.setAttribute('class', 'visible');
+document.getElementById('toggle').onclick = function(e) {
+  e.preventDefault();
+  var notice = document.getElementById('notice');
+  if (notice.className === "hidden") {
+    notice.setAttribute('class', 'visible');
   }
-  else if (target.className === "visible") {
-    target.setAttribute('class', 'hidden');
+  else if (notice.className === "visible") {
+    notice.setAttribute('class', 'hidden');
   }
-}
+};
+
+// 4.
+
+document.getElementById('notice').onclick = function() {
+  if (this.className === "visible") {
+    this.setAttribute('class','hidden')
+  }
+};
+
+// 5. Change html
+document.getElementById('multiplication').innerHTML = "117";
+
+
+// 6. Apply class to body
+document.body.setAttribute('id','styled');
