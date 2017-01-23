25f05a9bf63c89035c375888d2e7e8573be2f963
dylankb <dylan.barnard@gmail.com>
2017-01-23 08:16:59 -0800

    WIP js game search

--- js/ls/220/lesson_4/3_games/games.js
+++ js/ls/220/lesson_4/3_games/games.js
@@ -1,3 +1,17 @@
-label {
-  display: block;
-}
+$(function() {
+
+  var $categories = $('input');
+  var $titles = $('li');
+
+  $categories.on('change', function() {
+    var $checkbox = $(this);
+    var category = $checkbox.val();
+    var checked = $checkbox.is(":checked");
+
+    // if ($(this).is(':checked')) {
+    //   $('li').filter(':contains(' + title + ')').show();
+    // } else {
+    //   $('li').filter(':contains(' + title + ')').hide();
+    // }
+  });
+});
