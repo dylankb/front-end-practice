e996bcf97e3f8eb3fba121c31bd38b1848050a1f
dylankb <dylan.barnard@gmail.com>
2017-01-25 22:09:09 -0800

    Create game search functionality

--- js/ls/220/lesson_4/3_games/games.js
+++ js/ls/220/lesson_4/3_games/games.js
@@ -1,5 +1,47 @@
 $(function() {
 
+  var catalog = [{
+    "title": "The Legend of Zelda: Majora's Mask 3D",
+    "id": 1,
+    "category": "Nintendo 3DS"
+  }, {
+    "title": "Super Smash Bros.",
+    "id": 2,
+    "category": "Nintendo 3DS"
+  }, {
+    "title": "Super Smash Bros.",
+    "id": 3,
+    "category": "Nintendo WiiU"
+  }, {
+    "title": "LEGO Batman 3: Beyond Gotham",
+    "id": 4,
+    "category": "Nintendo WiiU"
+  }, {
+    "title": "LEGO Batman 3: Beyond Gotham",
+    "id": 5,
+    "category": "Xbox One"
+  }, {
+    "title": "LEGO Batman 3: Beyond Gotham",
+    "id": 6,
+    "category": "PlayStation 4"
+  }, {
+    "title": "Far Cry 4",
+    "id": 7,
+    "category": "PlayStation 4"
+  }, {
+    "title": "Far Cry 4",
+    "id": 8,
+    "category": "Xbox One"
+  }, {
+    "title": "Call of Duty: Advanced Warfare",
+    "id": 9,
+    "category": "PlayStation 4"
+  }, {
+    "title": "Call of Duty: Advanced Warfare",
+    "id": 10,
+    "category": "Xbox One"
+  }];
+
   var $categories = $('input');
   var $titles = $('li');
 
@@ -8,10 +50,21 @@ $(function() {
     var category = $checkbox.val();
     var checked = $checkbox.is(":checked");
 
+    // First solution (text matching)
     // if ($(this).is(':checked')) {
-    //   $('li').filter(':contains(' + title + ')').show();
+    //   $('li').filter(':contains(' + category + ')').show();
     // } else {
-    //   $('li').filter(':contains(' + title + ')').hide();
+    //   $('li').filter(':contains(' + category + ')').hide();
     // }
+
+    // Official solution (match by data-id)
+    var $categoryItems = catalog.filter(function(item) {
+      return item.category === category;
+    });
+
+    categoryItems.forEach(function(item) {
+      $items.filter("[data-id=" + item.id + "]").toggle(checked);
+    });
+
   });
 });
