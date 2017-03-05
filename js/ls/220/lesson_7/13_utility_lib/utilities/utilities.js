72b963ffd9dd13e3e0d532e95d78f094d07e2265
dylankb <dylan.barnard@gmail.com>
2017-03-04 19:52:00 -0800

    Finish JS utility library

--- js/ls/220/lesson_7/13_utility_lib/utilities/utilities.js
+++ js/ls/220/lesson_7/13_utility_lib/utilities/utilities.js
@@ -1,4 +1,20 @@
 (function() {
+  function newModifiedObject(args, element, { pick, omit } = {}) {
+    var obj = {};
+    args.forEach(function(key) {
+      for (var prop in element) {
+        if (prop === key) {
+          if (pick) { obj[key] = element[key]; }
+        }
+        if (prop !== key) {
+          if (omit) { obj[key] = element[key]; }
+        }
+      }
+    });
+
+    return obj;
+  }
+
   var _ = function(element) {
     u = {
       first: function() {
@@ -22,18 +38,93 @@
       sample: function() {
         var arr = [];
         var args = Array.prototype.slice.call(arguments);
+        var size = args[0];
+
+        function getRandomEle(size) {
+          return Math.floor(Math.random() * size);
+        }
+
         if (args.length === 0) {
           return element[0];
         } else {
-          for (var i=0; i < element.length; i++) {
-            if (arr.indexOf(element[i]) === -1) {
-              arr.push(element[i]);
+          while (arr.length < size) {
+            index = getRandomEle(size);
+            if (arr.indexOf(element[index]) === -1) {
+              arr.push(element[index]);
             }
           }
         }
         return arr;
       },
+      findWhere: function(searchObj) {
+        var match;
+
+        element.some(function(obj) {
+          var allMatch = true;
+
+          for (var prop in searchObj) {
+            if (obj[prop] !== searchObj[prop]) {
+              allMatch = false;
+            }
+          }
+
+          if (allMatch) {
+            match = obj;
+            return true;
+          }
+        });
+
+        return match;
+      },
+      where: function(searchObj) {
+        var arr = [];
+
+        element.forEach(function(obj) {
+          for (var prop in searchObj) {
+            if (obj[prop] === searchObj[prop]) {
+              arr.push(obj);
+            }
+          }
+        });
+
+        return arr;
+      },
+      pluck: function(searchKey) {
+        var arr = [];
+
+        element.forEach(function(obj) {
+          if (obj[searchKey]) {
+            arr.push(obj[searchKey]);
+          }
+        });
+
+        return arr;
+      },
+      keys: function() {
+        return Object.keys(element);
+      },
+      values: function() {
+        return Object.values(element);
+      },
+      pick: function() {
+        var args = [].slice.call(arguments);
+
+        return newModifiedObject(args, element, { pick: true });
+      },
+      omit: function() {
+        var args = [].slice.call(arguments);
+
+        return newModifiedObject(args, element, { omit: true });
+      },
+      has: function(property) {
+        return Object.prototype.hasOwnProperty.call(element, property);
+      },
     };
+
+    (["isElement", "isArray", "isObject", "isBoolean", "isString", "isFunction", "isNumber"]).forEach(function(method) {
+      u[method] = function() { _[method].call(u, element); };
+    });
+
     return u;
   };
 
@@ -48,6 +139,60 @@
       arr.push(i);
     }
     return arr;
+  };
+
+  _.extend = function() {
+    var args = [].slice.call(arguments);
+
+    var oldObj = args[0];
+    var newObjs = args.slice(1);
+
+    newObjs.forEach(function(obj) {
+      for (var prop in obj) {
+        oldObj[prop] = obj[prop];
+      }
+    });
+
+    return oldObj;
+  };
+
+  _.isElement = function(ele) {
+    return ele && ele.nodeType === 1;
+  };
+
+  _.isArray = Array.isArray || function(obj) {
+    return toString.call(obj) === "[object Array]";
+  };
+
+  _.isObject = function(obj) {
+    var type = typeof obj;
+
+    return type === "function" || type === "object" && !!obj;
+  };
+
+  _.isFunction = function(obj) {
+    var type = typeof obj;
+
+    return type === "function";
+  };
+
+  (["Boolean", "String", "Number"]).forEach(function(method) {
+    _["is" + method] = function(obj) {
+      return toString.call(obj) === ["object " + method];
+    };
+  });
+
+  _.isBoolean = function(obj) {
+    return toString.call(obj) === "[object Boolean]";
+    // return obj === true || false; // Doesn't account for primitives - new Boolean(false)
+  };
+
+  _.isString = function(obj) {
+    return toString.call(obj) === "[object String]";
+  };
+
+  _.isNumber = function(obj) {
+    return toString.call(obj) === "[object Number]";
   };
 
   window._ = _;
