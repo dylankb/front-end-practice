2e5f6904432d6dbb9772a9c644ab5620b5877c09
dylankb <dylan.barnard@gmail.com>
2017-05-16 15:17:07 -0700

    Commit notes on js

--- js/ls/220/lesson_4/5_animating_shapes/animations.js
+++ js/ls/220/lesson_4/5_animating_shapes/animations.js
@@ -1,70 +1,66 @@
+function getFormObject($form) {
+  var $inputs = $form.serializeArray();
+
+  return $inputs.reduce(function(formObject, item) {
+    formObject[item.name] = item.value;
+    return formObject;
+  }, {});
+
+}
+
+function createElement(data) {
+  // return '<div ' + 'data-start-x=' + $formObject.startX +
+  //                 ' data-start-y=' + $formObject.startY +
+  //                 ' data-end-x=' + $formObject.endX +
+  //                 ' data-end-y=' + $formObject.endY +
+  //                 ' class="shapes ' + shapeClass +
+  //                 '"></div>';
+
+  var $element = $("<div />", {
+    "class": 'shapes ' + data.shapeType,
+    data: data // Replaces calling $d.data(data) after $element definition
+  });
+  resetElement($element);
+
+  return $element;
+}
+
+function resetElement($e) {
+  var data = $e.data();
+
+  $e.css({
+    top: +data.startY,
+    right: +data.startX,
+  });
+}
+
+function animateElement() {
+  var $e = $(this);
+  var data = $e.data();
+
+  resetElement($e);
+
+  $e.animate({
+    top: +data.endY,
+    left: +data.endX
+  }, 1000);
+}
+
+function stopAllAnimations($canvas) {
+  $canvas.find('.shapes').stop();
+}
+
 $(function() {
 
   var $canvas = $('.shapes-container');
 
-  function getFormObject($form) {
-    var $inputs = $form.serializeArray();
-
-    return $inputs.reduce(function(formObject, item) {
-      formObject[item.name] = item.value;
-      return formObject;
-    }, {});
-
-  }
-
-  function createElement(data) {
-    // return '<div ' + 'data-start-x=' + $formObject.startX +
-    //                 ' data-start-y=' + $formObject.startY +
-    //                 ' data-end-x=' + $formObject.endX +
-    //                 ' data-end-y=' + $formObject.endY +
-    //                 ' class="shapes ' + shapeClass +
-    //                 '"></div>';
-
-    var $element = $("<div />", {
-      "class": 'shapes ' + data.shapeType,
-      data: data // Replaces calling $d.data(data) after $element definition
-    });
-
-    resetElement($element);
-
-    return $element;
-  }
-
-  function resetElement($e) {
-    var data = $e.data();
-
-    $e.css({
-      top: +data.startY,
-      right: +data.startX,
-    });
-  }
-
-  function animateElement() {
-    var $e = $(this);
-    var data = $e.data();
-
-    resetElement($e);
-
-    $e.animate({
-      top: +data.endY,
-      left: +data.endX
-    }, 1000);
-  }
-
-  function stopAllAnimations() {
-    $canvas.find('.shapes').stop();
-  }
-
   $('form').on('submit', function(e) {
     e.preventDefault();
-
     $f = $(this);
-
     var $formObject = getFormObject($f);
     console.log($formObject);
 
     var $shapeElement = createElement($formObject);
-
     console.log($shapeElement);
 
     $('.shapes-container').append($shapeElement);
@@ -74,12 +70,12 @@ $(function() {
 
   $('.start').on('click', function(e) {
     e.preventDefault();
-    stopAllAnimations();
+    stopAllAnimations($canvas);
 
     $canvas.find('.shapes').each(animateElement);
   });
 
   $('.end').on('click', function(e) {
-    stopAllAnimations();
+    stopAllAnimations($canvas);
   });
 });
