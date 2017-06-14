bac8e41a27eb1745d19d66a5e3a1f68e2862d2e1
dylankb <dylan.barnard@gmail.com>
2017-06-13 21:59:34 -0700

    Basic setup for express  app

---
+++ js/ls/260/express_and_preprocessors/express_app/routes/all.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var _ = require("underscore");
+
+
+module.exports = function(app) {
+  function setActiveNavTo(title) {
+    var activeItem =  _(app.locals.links).findWhere({ active: true });
+    if (activeItem) { activeItem.active = false; }
+    _.findWhere(app.locals.links, { title: title }).active = true;
+  }
+
+  /* GET home page. */
+  router.get('/', function(req, res, next) {
+    var title = "Home";
+
+    setActiveNavTo(title)
+    res.render('index', {
+      title: title,
+    });
+  });
+
+  router.get('/about', function(req, res, next) {
+    var title = "About";
+
+    setActiveNavTo(title);
+
+    res.render('about', {
+      title: title,
+    });
+  });
+
+  return router;
+}
