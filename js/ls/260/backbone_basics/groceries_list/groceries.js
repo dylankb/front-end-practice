fe69e0ea9e5c4163b65a20df367d3c6fedfb8445
dylankb <dylan.barnard@gmail.com>
2017-07-09 16:38:27 -0700

    Grocery App - multiple models handling
    Removed editing functionality, but a user can now add, delete models

--- js/ls/260/backbone_basics/groceries_list/groceries.js
+++ js/ls/260/backbone_basics/groceries_list/groceries.js
@@ -1,39 +1,78 @@
-var FoodModel = Backbone.Model.extend({
-  initialize: function() {
-    this.set('date_formatted', moment().format('MMMM Do YYYY, h:mm:ss'));
-    this.set('datetime', moment().format('YYYY-MM-DD[T]hh:mm:ss'));
-
-    this.on('change', this.render);
-    this.render();
+var Items = {
+  init: function() {
+    Handlebars.registerPartial('item', $('#item').html());
+    this.seedCollection();
+    this.bindEvents();
   },
-  renderArticle: function() {
-    $('article').html(templates.product(this.toJSON()));
-  },
-  renderForm: function() {
-    $('form fieldset').html(templates.form(this.toJSON()));
-  },
-  render: function() {
-    this.renderArticle();
-    this.renderForm();
-  },
-  updateFood: function(e) {
+  addItem: function(e) {
     e.preventDefault();
 
     var $form = $(e.target).closest('form');
-    var newProps = {
+    var itemData = {
       "name": $form.find('input[name=name]').val(),
-      "description": $form.find('textarea[name=description]').val(),
+      "quantity": $form.find('input[name=quantity]').val(),
     };
 
-    this.set(newProps);
+    var item = new ItemModel(itemData);
+    this.collection.push(item);
+  },
+  bindEvents: function() {
+    $('form').on('submit', this.addItem.bind(this));
+    $('tbody').on('click', 'a', this.deleteItem.bind(this));
+    $('p a').on('click', this.deleteAll.bind(this));
+    $('thead th').on('click', this.sortSelect.bind(this));
+  },
+  collection: [],
+  deleteAll: function(e) {
+    this.collection = [];
+    this.render();
+  },
+  deleteItem: function(e) {
+    e.preventDefault();
+
+    var target = e.target
+    var id = Number(target.dataset.id);
+    var item = _.find(this.collection, function(item) {
+      return item.get('id') === id;
+    });
+
+    this.collection = _.without(this.collection, item);
+    $(target).closest('tr').remove();
+  },
+  lastID: 0,
+  template: Handlebars.compile($('#items').html()),
+  render: function() {
+    $('tbody').html(this.template({ items: this.collection }));
+  },
+  seedCollection: function() {
+    items_json.forEach(function(item) {
+      var newItem = new ItemModel(item);
+      this.collection.push(newItem);
+    }, this);
+  },
+  sortSelect: function(e) {
+    e.preventDefault();
+
+    var prop = e.target.dataset.prop;
+    this.collection = _.sortBy(this.collection, function(item) {
+      return item.get(prop);
+    });
+    this.render();
+  }
+};
+
+var ItemModel = Backbone.Model.extend({
+  initialize: function() {
+    this.setID();
+    this.render();
+  },
+  render: function(data) {
+    $('tbody').append(this.template(this.toJSON()));
+  },
+  template: Handlebars.compile($('#item').html()),
+  setID: function() {
+    this.set('id', ++Items.lastID);
   }
 });
 
-var templates = {};
-$('script[type="text/x-handlebars"]').each(function() {
-  templates[this.id] = Handlebars.compile($(this).html());
-});
-
-var food = new FoodModel(product_json);
-
-$('form').on('submit', food.updateFood.bind(food));
+Items.init();
