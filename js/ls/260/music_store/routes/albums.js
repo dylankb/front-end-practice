715744c112d6c8f4c9182d82e72c412f0fb0ec87
dylankb <dylan.barnard@gmail.com>
2017-07-15 17:22:52 -0700

    Add ability to submit new albums

--- js/ls/260/music_store/routes/albums.js
+++ js/ls/260/music_store/routes/albums.js
@@ -3,18 +3,30 @@ var fs = require('fs')
 var filePath = path.resolve(path.dirname(__dirname), 'data/albums.json');
 
 function getAlbums() {
-  return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  return JSON.parse(fs.readFileSync(filePath, 'utf8')).data;
 }
 
-function getFormData() {
-  $('#album_new').find('form').serializeForm().reduce(function(acc, input) {
-    return acc.input.key = input.value;
-  }, {});
+function nextID() {
+  return JSON.parse(fs.readFileSync(filePath, "utf8")).last_id + 1;
+}
+
+function writeAlbums(data) {
+  fs.writeFileSync(filePath, JSON.stringify(data), "utf8")
 }
 
 module.exports = function(router) {
+  router.post("/albums", function(req, res) {
+    var album = req.body;
+    var albums = getAlbums();
+
+    album.id = nextID();
+    albums.push(album);
+    writeAlbums({ lastID: album.id, data: albums }); // update data in file
+    res.json(album);// send a response back
+  });
+
   /* GET /albums/new. */
-  router.get('/albums/new', function(req, res) { 
+  router.get('/albums/new', function(req, res) {
     res.render('new');
   });
 }
--- js/ls/260/music_store/routes/index.js
+++ js/ls/260/music_store/routes/index.js
@@ -1,12 +1,10 @@
-var express = require('express');
 var path = require('path'); // sets a normalized path for the file. As long as view directory relative to that file, the path will be correct
 
 var fs = require('fs')
 var filePath = path.resolve(path.dirname(__dirname), 'data/albums.json');
-var router = express.Router();
 
 function getAlbums() {
-  return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  return JSON.parse(fs.readFileSync(filePath, 'utf8')).data;
 }
 
 module.exports = function(router) {
