e793611c8002ba2c5ba5422793d4f75894372fc2
dylankb <dylan.barnard@gmail.com>
2017-07-15 21:30:19 -0700

    Refactor album methods to  node module

--- js/ls/260/music_store/routes/albums.js
+++ js/ls/260/music_store/routes/albums.js
@@ -1,28 +1,34 @@
 var path = require('path');
-var fs = require('fs')
-var filePath = path.resolve(path.dirname(__dirname), 'data/albums.json');
-
-function getAlbums() {
-  return JSON.parse(fs.readFileSync(filePath, 'utf8')).data;
-}
-
-function nextID() {
-  return JSON.parse(fs.readFileSync(filePath, "utf8")).last_id + 1;
-}
-
-function writeAlbums(data) {
-  fs.writeFileSync(filePath, JSON.stringify(data), "utf8")
-}
+var _ = require('underscore');
+var Albums = require(path.resolve(path.dirname(__dirname), "modules/albums"));
+// Relative path for the module
 
 module.exports = function(router) {
-  router.post("/albums", function(req, res) {
+  router.route("/albums").get(function(req, res) {
+    res.json(Albums.get());
+  }).post(function(req, res) {
     var album = req.body;
-    var albums = getAlbums();
+    var albums = Albums.get();
 
-    album.id = nextID();
+    album.id = Albums.getLastId() + 1;
     albums.push(album);
-    writeAlbums({ lastID: album.id, data: albums }); // update data in file
-    res.json(album);// send a response back
+    Albums.set(albums) // update data in file
+    res.json(album);  // send a response back
+  }).put(function(req, res) {
+    var albums = Albums.get();
+    var currentAlbum = _(albums).findWhere({ id: req.body.id });
+
+    _.extend(currentAlbum, req.body);
+    Albums.set(albums);
+    res.json(currentAlbum);
+  }).delete(function(req, res) {
+    var albums = _(Albums.get()).reject(function(a) {
+      return a.id === +req.body.id;
+    });
+
+    Albums.set(albums);
+    res.status(200).end();
+    // Response on delete method can be success since will be using ajax.
   });
 
   /* GET /albums/new. */
--- js/ls/260/music_store/routes/index.js
+++ js/ls/260/music_store/routes/index.js
@@ -1,15 +1,10 @@
 var path = require('path'); // sets a normalized path for the file. As long as view directory relative to that file, the path will be correct
-
-var fs = require('fs')
-var filePath = path.resolve(path.dirname(__dirname), 'data/albums.json');
-
-function getAlbums() {
-  return JSON.parse(fs.readFileSync(filePath, 'utf8')).data;
-}
+var Albums = require(path.resolve(path.dirname(__dirname), "modules/albums.js"));
 
 module.exports = function(router) {
   /* GET home page. */
   router.get('/', function(req, res, next) {      // responding to index path
-    res.render('index', { albums: getAlbums() }); // take index jade view, and pass it data
+    res.render('index', { albums: Albums.get() }); // take index jade view, and pass it data
   });
+
 }
