f83dd2d0560f8f06efa97fb21665fb4aa0918e41
dylankb <dylan.barnard@gmail.com>
2017-07-22 13:10:04 -0700

    Add ability to add multiple of same object type to cart

--- js/ls/260/music_store/public/javascripts/collection/cart.js
+++ js/ls/260/music_store/public/javascripts/collection/cart.js
@@ -4,19 +4,27 @@ var CartItems = Backbone.Collection.extend({
     this.total = this.toJSON().reduce(function(acc, album) {
       return acc + album.price * album.quantity;
     }, 0);
+
+    return this;
   },
   getQuantity: function() { return this.quantity; },
   setQuantity: function() {
     this.quantity = this.toJSON().reduce(function(acc, album) {
       return acc + album.quantity;
     }, 0);
+
+    return this;
   },
   addItem: function(item) {
-    item = item.clone();
-    item.set('quantity', 1);
-    this.add(item);
-    this.setQuantity();
-    this.setTotal();
+    var existing = this.get(item.get('id')); // Check for existing item in collection
+    if (existing) {
+      existing.set('quantity', existing.get('quantity') + 1);
+    } else {
+      item = item.clone();
+      item.set('quantity', 1);
+      this.add(item);
+    }
+    this.setQuantity().setTotal();
     this.trigger('CART_UPDATED');
   },
 });
