e65d8d20a130f9292c1be7032b26616f76b7031b
dylankb <dylan.barnard@gmail.com>
2017-07-27 22:10:32 -0700

    Allow user to delete album

--- js/ls/260/music_store/public/javascripts/view/index.js
+++ js/ls/260/music_store/public/javascripts/view/index.js
@@ -14,7 +14,22 @@ var IndexView = Backbone.View.extend({
     e.preventDefault();
     this.trigger("SHOW_NEW_ALBUM")
   },
+  deleteAlbum: function(e) {
+    e.preventDefault();
+    var $link = $(e.target)
+    var albumId = Number($link.data('id'));
+
+    $.ajax({
+      url: '/albums',
+      method: 'delete',
+      data: { id: albumId },
+      success: function() {
+        $link.closest('li').remove();
+      }
+    });
+  },
   events: {
-    'click .showForm': 'renderForm'
+    'click .showForm': 'renderForm',
+    'click .album-remove-index': 'deleteAlbum',
   },
 });
