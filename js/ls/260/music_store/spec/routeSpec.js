93c2e898b8696dfa32d9efe05d1f45cde5fb9a56
dylankb <dylan.barnard@gmail.com>
2017-08-19 22:16:07 -0700

    Create initial route spec

--- js/ls/260/music_store/spec/routeSpec.js
+++ js/ls/260/music_store/spec/routeSpec.js
@@ -12,7 +12,34 @@ describe('Music store', function() {
       });
     });
   });
-  describe('GET /', function() {
-    // it('')
+  describe('POST /', function() {
+    // var testBody = {
+    //   title: 'Another New Song',
+    // };
+    var testBody = { title: 'Test Title',
+      artist: 'Test Artist',
+      date: '8888-08-08',
+      cover: '',
+      price: 'Test',
+    };
+
+    it('returns JSON response', function(done) {
+      request.post(root + 'albums', testBody, function(e, res) {
+        var id = res.body.id;
+
+        expect(res.statusCode).toEqual(200);
+        expect(id).toBeDefined();
+        done();
+      });
+    });
+    describe('GET /albums', function() {
+      it('returns a valid 200 response', function(done) {
+        request(root + 'albums', function(e, res) {
+          console.log(res.body);
+          expect(res.statusCode).toEqual(200);
+          done();
+        });
+      });
+    });
   });
 });
