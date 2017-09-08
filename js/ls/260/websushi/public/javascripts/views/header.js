a4ca8834d520cf9a35b1f34c1cebc67600075531
dylankb <dylan.barnard@gmail.com>
2017-09-07 17:12:11 -0700

    Set header el from options

--- js/ls/260/websushi/public/javascripts/views/header.js
+++ js/ls/260/websushi/public/javascripts/views/header.js
@@ -1,11 +1,11 @@
 var HeaderView = Backbone.View.extend({
+  el: '.page-header',
   initialize: function() {
     this.render();
     this.listenTo(this.collection, 'add reset', this.render);
   },
   render: function() {
     this.$el.html(this.template({ count: this.collection.size() }));
-    $('.page-header').html(this.$el);
   },
   template: Handlebars.templates.header,
 });
