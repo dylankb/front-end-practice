4dcfc7309bd8af7d1b05140b6d2aef5b2692535e
dylankb <dylan.barnard@gmail.com>
2017-09-08 15:33:46 -0700

    Fix failure to render cart summary

--- js/ls/260/websushi/public/javascripts/collections/cart.js
+++ js/ls/260/websushi/public/javascripts/collections/cart.js
@@ -2,7 +2,6 @@ var CartCollection = Backbone.Collection.extend({
   initialize: function() {
     this.readStorage();
     this.View = new CartView({ collection: this });
-    this.on('remove change reset', this.update);
     this.on('ADD_ITEM', this.addItem);
     this.on('REMOVE_ITEM', this.removeItem);
     this.on('EMPTY_CART', this.emptyCart);
@@ -16,10 +15,12 @@ var CartCollection = Backbone.Collection.extend({
       this.add(item);
       item.set('quantity', 1);
     }
+    this.update();
   },
   emptyCart: function() {
     this.reset();
     this.setStorage();
+    this.update();
   },
   readStorage: function() {
     var existingCart = JSON.parse(window.localStorage.getItem('cart'));
@@ -36,6 +37,7 @@ var CartCollection = Backbone.Collection.extend({
     } else {
       item.set('quantity', quantity - 1);
     }
+    this.update();
   },
   setCartTotal: function() {
     this.total = this.models.reduce(function sumCartTotal(acc, item) {
